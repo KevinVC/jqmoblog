03ff6be07f93416607f8d4646f2661faf8df61c0
Kevin Vega <kevinvcm@gmail.com>
2012-03-21 17:16:06 -0500

    lib jquerymobile and views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmoblog::Application.config.secret_token = '40484fd9ebecbf87f5732ae906397c360fd2614bfc254a8168b14a00ffe9707dffb63deea7bb40939808cd007df337aee7b0f879e7460477e1ba3160ad60a2bb'
